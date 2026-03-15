c6042aeaf1eb8d925c9e29762a3208b56358ae4c
Consenium OS <gjhcftftvhhfssdf@gmail.com>
2026-03-15 22:29:01 +0700

    Create site.js

---
+++ src/config/site.js
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "Consenium OS",
+  description:
+    "The central command for your digital footprint in the AI era.",
+  twitter: "https://x.com/ConseniumDao",
+  website: "https://consenium.xyz/"
+};
